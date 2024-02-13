11ab552c8ba3ae541c44cb7bbfc9db07c8627cbf
Max <60605508+karuzoXam@users.noreply.github.com>
2024-02-13 14:40:53 +0100

    added default icon if none is given

--- ComponentLibrary/src/Components/Cards/Card.jsx
+++ ComponentLibrary/src/Components/Cards/Card.jsx
@@ -1,6 +1,14 @@
 import './Card.css';
 
-export default function Card({ icon, iconColor, title, text }) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCloudArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+export default function Card({
+  icon = <FontAwesomeIcon icon={faCloudArrowUp} />,
+  iconColor,
+  title,
+  text,
+}) {
   return (
     <div className="card">
       <div className="card-icon" style={{ backgroundColor: iconColor }}>
